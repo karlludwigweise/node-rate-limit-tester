a8e5ed77962833227053f4058bfbd833d040739a
Karl Ludwig Weise <office@ludwigweise.de>
2020-10-12 22:04:35 +0200

    initial commit

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# Rate Limit Tester
+
+Install Dependencies: `yarn`.
+Usage: `node ./ -n 20 -u {your-url}`.
---
+++ index.js
@@ -0,0 +1,23 @@
+import fetch from "node-fetch";
+import chalk from "chalk";
+import yargs from "yargs";
+
+const params = yargs(process.argv.slice(2)).argv;
+const length = params.n || 10;
+const requests = new Array(length).fill(null);
+
+if (params.u) {
+  const numLength = `${params.n}`.length;
+  console.log(chalk.bgRgb(255, 140, 105)(params.u));
+  requests.map(async (x, i) => {
+    const reqNo = `#${`${i}`.padStart(numLength, "0")}`;
+    const resp = await fetch(params.u);
+    if (resp.status === 200) {
+      console.log(reqNo, chalk.green(resp.status));
+    } else {
+      console.log(reqNo, chalk.bgRedBright(resp.status));
+    }
+  });
+} else {
+  console.log(chalk.red("Param -u is not set"));
+}
